e6c6b2dee426bc95cc59caf918716cd39a7e768e
Rafael <rafaelroma@timepix.com.br>
2021-02-17 16:54:56 -0300

    section C + ajustes

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const mainPurple: string = "#8c52e5";
 export const mainGreen: string = "#68de5a";
+export const FontDefault: string =  `font-family: 'Roboto', sans-serif;`;
 
 export default createGlobalStyle`
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
@@ -24,11 +25,25 @@ html {
 }
 
 body {
-  min-height: 100vh;
-  background: ${mainPurple};
-  font-weight: 400;
-  font-family: "Roboto", sans-serif;
-}
+        position: relative;
+        display: block;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        ${FontDefault}
+        background: ${mainPurple};
+    }
+
+    p {
+      padding: 0;
+      margin: 0;
+    } 
+    
+    h1, h2, h3, h4, h5 {
+      padding: 0;
+      margin: 0;
+    }
 
 button {
   cursor: pointer;
--- src/views/pages/Landing/index.tsx
+++ src/views/pages/Landing/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-//
+
 import SectionA from "./SectionA";
+import SectionC from "./SectionC";
 
 const Landing: React.FC = () => {
   return (
     <>
       <SectionA />
+      <SectionC />
     </>
   );
 };
