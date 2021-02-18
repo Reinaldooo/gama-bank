208b3995e5e18aaed13da2f030db78a2f8d57030
jequelia <jequelia@gmail.com>
2021-02-18 15:16:14 -0300

    feat: criação da sessão D da página home

--- src/views/pages/Landing/index.tsx
+++ src/views/pages/Landing/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 //
 import SectionA from "./SectionA";
+import SectionD from "./sectionD";
+
 
 const Landing: React.FC = () => {
   return (
     <>
-      <SectionA />
+      <SectionA/>
+      <SectionD/>
     </>
   );
 };
--- src/views/pages/Landing/sectionD/styles.ts
+++ src/views/pages/Landing/sectionD/styles.ts
@@ -1,43 +1,54 @@
 import styled from "styled-components";
+import img from '../../../../assets/picture-section-d.jpg'
 
 export const Section = styled.section `
-.section-d{
+
     height: 100vh;
     width: 100%;
     margin: 0;
+    position: relative;
+    display: block;
+    
+    .container{
+        height: 100vh;
+       
+        .img-paralax{
+            background-image: url(${img});
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 100vh;
+
+
+            .description{
+                background-color: #65f054b5;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                height: 100vh;
+                color: #FFFF;
+        
+                h1{
+                    font-size: 69px;
+                    margin-bottom: 2%;
+                    margin-left: 5%;
+                }
+        
+                h6{
+                    font-weight: 200;
+                    font-size: 23px;
+                    width: 28%;
+                    margin: 0;
+                    margin-left: 5%;
+                }
+        
+        
+            }
+        }
+
 }
 
-.background-img-paralax{
-    background-image: url(../../../assets/picture-section-d.jpg);
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 100%;
-}
 
-.background-color{
-    height: 100%;
-    background-color: #65f054b5;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
 
-.title{
-    font-size: 69px;
-    margin-bottom: 2%;
-}
-
-.description{
-    font-weight: 200;
-    font-size: 23px;
-    width: 28%;
-    margin: 0;
-}
-
-.text{
-    margin-left: 5%;
-    color: #FFFFFF;
-}
 `
