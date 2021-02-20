295adab50297ae841ae905f3dee97721deb93a6b
Rafael Roma <rafaelroma@timepix.com.br>
2021-02-20 11:32:09 -0300

    criacao do component ButtonGeneric para utilizar no projeto + alterações index e login

--- src/views/pages/Landing/SectionA/index.tsx
+++ src/views/pages/Landing/SectionA/index.tsx
@@ -1,40 +1,40 @@
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 
 //
 import gamaLogo from "../../../../assets/gama-academy-logo-horizontal-verde-branco1 1.png";
-import ButtonPrimary from "../../../components/ButtonPrimary";
 import WhiteCardHome from "../../../components/WhiteCardHome";
 import * as S from "./styles";
 import CreateAccountForm from "./CreateAccountForm";
+import {FiChevronRight} from "react-icons/fi";
+import ButtonGeneric from "../../../components/ButtonGeneric";
 
 const SectionA: React.FC = () => {
-  const history = useHistory();
+    const history = useHistory();
 
-  return (
-    <S.Container>
-      <S.Header>
-        <Link to="/" href="/">
-          <img src={gamaLogo} alt="logo" />
-        </Link>
-      </S.Header>
-      <S.Content>
-        <S.LeftContent>
-          <span>Gama Bank é um projeto de nossos alunos.</span>
-          <span>Já tem conta?</span>
-          <ButtonPrimary
-            title="Acessar"
-            onClick={() => history.push("/login")}
-          />
-        </S.LeftContent>
-        <S.RightContent>
-          <WhiteCardHome title="Peça sua conta e cartão de crédito Gama Bank">
-            <CreateAccountForm />
-          </WhiteCardHome>
-        </S.RightContent>
-      </S.Content>
-    </S.Container>
-  );
+    return (
+        <S.Container>
+            <S.Header>
+                <Link to="/" href="/">
+                    <img src={gamaLogo} alt="logo"/>
+                </Link>
+            </S.Header>
+            <S.Content>
+                <S.LeftContent>
+                    <span>Gama Bank é um projeto de nossos alunos.</span>
+                    <span>Já tem conta?</span>
+                    <ButtonGeneric title="Acessar" onClick={() => history.push("/login")} _width="60%" _padding="15px 25px" _fSize="18px"
+                                   _bgColor="#FFFFFF" _color="#8C52E5" _colorHover="#8C52E5"
+                                   _bgHover="#68DE5A" _marTop="30px" icon={FiChevronRight}/>
+                </S.LeftContent>
+                <S.RightContent>
+                    <WhiteCardHome title="Peça sua conta e cartão de crédito Gama Bank">
+                        <CreateAccountForm/>
+                    </WhiteCardHome>
+                </S.RightContent>
+            </S.Content>
+        </S.Container>
+    );
 };
 
 export default SectionA;
