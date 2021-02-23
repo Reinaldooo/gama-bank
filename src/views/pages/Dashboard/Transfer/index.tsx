12d1a665c7fd967230b42f75f158c715eca79958
jequelia <jequeliasantana@gmail.com>
2021-02-23 16:36:22 -0300

    adicionado lógica de transferencia

--- src/views/pages/Dashboard/Transfer/index.tsx
+++ src/views/pages/Dashboard/Transfer/index.tsx
@@ -5,33 +5,42 @@ import api from '../../../../services/api'
 import { Container, Header } from './style';
 import WhiteCardDash from '../../../components/WhiteCardDashboard';
 import { FiChevronLeft } from 'react-icons/fi';
+import { isAuth } from '../../../../services/auth';
 
-
-const Deposit: React.FC = () => {
-
+const Transfer: React.FC = () => {
     const [date, setDate] = useState('')
     const [description, setDescription] = useState('')
     const [valuer, setValuer ] = useState('')
+    const [recipient, setRecipient ] = useState('')
+    const [type, setType ] = useState('840')
+
+
 
     const history = useHistory()
 
-    function deposit (event: FormEvent<HTMLFormElement>){
+    function transfer (event: FormEvent<HTMLFormElement>){
         event.preventDefault()
 
-        const postData = {
-            conta: 676,
-            contaDestino: 'jeque',
+        const postData =  type === "840" ?{
+            conta: "675",
             data: date,
             descricao: description,
-            login: 'jeque',
+            login: isAuth().user,
             valor: valuer,
-            planoConta: 838 
+            planoConta: type 
+        } : {
+            conta: "675",
+            contaDestino: recipient,
+            data: date,
+            descricao: description,
+            login: isAuth().user,
+            valor: valuer,
+            planoConta: type 
         }
-
         console.log(postData);
-        
         try {
-            api.post(`lancamentos`, postData, {headers: {Authorization: localStorage.getItem('@tokenApp')}} ).then(
+            const token =  localStorage.getItem('@tokenApp');
+            api.post(`lancamentos`, postData, {headers: {Authorization: token}} ).then(
               response => { 
                   console.log(response);
                 if (response.status === 200){
@@ -47,27 +56,34 @@ const Deposit: React.FC = () => {
     }
 
   return (
-    
     <Container>
       <Header>
         <Link to="/dashboard"> <FiChevronLeft size={30}/>Voltar</Link>
       </Header>
       <WhiteCardDash _maxWidth="100%">
-        <form onSubmit={deposit}>
-            <h3>Realize seu depósito</h3>
+        <form onSubmit={transfer}>
+            <h3>Realize sua transferência</h3>
             <input value={date} onChange={ e => setDate( e.target.value) } type="date" placeholder="Data"></input>
             <input value={description} onChange={ e => setDescription( e.target.value) } type="text" placeholder="Descrição"></input>
-            <input value={valuer} onChange={ e => setValuer( e.target.value) } type="number" placeholder="Valor do deposito"></input>
+            <input value={valuer} onChange={ e => setValuer( e.target.value) } type="number" placeholder="Qual o valor de sua transferência?"></input>
+            <select value={type} onChange={ e => setType( e.target.value) }>
+                <option  value="840"> Da minha conta para meu crédito</option>
+                <option value="841"> Tranferência da sua conta para outra conta</option>
+            </select>
+            {
+               type === "841" && 
+               <input value={recipient} onChange={ e => setRecipient( e.target.value) } type="text" placeholder="Login do destinatário"></input>
+
+            }
             <ButtonGeneric 
-            title="Realizar deposito agora" 
+            title="Realizar transferência agora" 
             type="submit" 
             _colorHover="#FFFFFF" 
             _bgHover="#3da131" />
         </form>
       </WhiteCardDash>
     </Container>
-  
     );
 }
 
-export default Deposit;
+export default Transfer;
