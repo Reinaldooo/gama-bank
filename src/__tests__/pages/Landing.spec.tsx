6c1768c2639c9bf84104d066c485caa12a9b5a5e
emamoura <emanuel.moura@ccc.ufcg.edu.br>
2021-02-25 21:16:39 -0300

    criacao da pasta de testes

---
+++ src/__tests__/pages/Landing.spec.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {fireEvent, getByText, render} from '@testing-library/react';
+import SectionA from "../../views/pages/Landing/SectionA"
+import SectionB from "../../views/pages/Landing/SectionB"
+import SectionC from "../../views/pages/Landing/SectionC"
+import SectionD from "../../views/pages/Landing/SectionC"
+ 
+jest.mock('react-router-dom', () => {
+    return {
+      useHistory: jest.fn(),
+      Link: ({children}: {children : React.ReactNode}) => children
+    }
+})
+
+
+describe('Check the render of the components of landing page', () => {
+    it('Should render the component Section A', () => {
+      const { debug } = render(<SectionA/>)
+      debug()
+    })  
+
+    it('Should render the component Section B', () => {
+      const { debug } = render(<SectionB/>)
+      debug()
+    })
+  
+    it('Should render the component Section C', () => {
+      const { debug } = render(<SectionC/>)
+      debug()
+    })
+  
+    it('Should render the component Section D', () => {
+      const { debug } = render(<SectionD/>)
+      debug()
+    })
+})
