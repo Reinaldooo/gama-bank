a35ab5a170544be1ba90484c613ad92a098ee857
Rafael Roma <rafaelroma@timepix.com.br>
2021-02-26 19:59:30 -0300

    ajustes styles home + ajustes SectionE

--- src/views/pages/Landing/SectionB/styles.ts
+++ src/views/pages/Landing/SectionB/styles.ts
@@ -1,77 +1,91 @@
 import styled from "styled-components";
 
 export const ContainerSection = styled.section`
-    min-height: 100vh;
-    width: 100%;
-    background-color: #EAEAEA;
-    padding: 1.5rem 0;
-    float: left;
-    display: flex;
-    justify-content: center;
-    align-items: center;   
-        
-    img {
-        max-width: 100%;
-    }
+  min-height: 100vh;
+  width: 100%;
+  background-color: #EAEAEA;
+  padding: 1.5rem 0;
+  float: left;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    max-width: 100%;
+  }
 `
 
 export const ContainerContent = styled.div`
-    width: 90%;
-    margin: auto;
-    display: flex;
-    justify-content: center;
-    
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+
+  h2 {
+    width: 100%;
+    text-align: center;
+    font-size: 32px;
+    padding: 0 20px;
+
+    @media (min-width: 0) and (max-width: 767px) {
+        width: 100%;
+        text-align: center;
+        font-size: 25px;
+        margin-bottom: 20px
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        width: 100%;
+        text-align: center;
+        font-size: 25px;
+        margin-bottom: 20px
+    }
+  }
+
+  h3 {
+    padding-left: 100px;
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 991px) {
+    flex-direction: column;
+
     h2, h3 {
-        padding-left: 5.625rem;
+      padding: 0;
+      text-align: center
     }
-
-    h2 {
-        font-size: 2rem;
-    }
-
-    h3 {
-        font-size: 1.25rem;
-    }
-    
-    @media screen and (max-width: 991px) {
-        flex-direction: column;
-        
-        h2, h3 {
-            padding: 0;
-            text-align: center
-        }
-    }
+  }
 `
 
 export const DarkPart = styled.section`
-    width: 50%;
-    padding: 3rem 0;
-    background-color: #000;
-    color: #68DE5A;
-    
-        & img {
-            margin-left: auto;
-        }
+  width: 50%;
+  padding: 3rem 0;
+  background-color: #000;
+  color: #68DE5A;
 
-    @media screen and (max-width: 991px) {
-        width: 100%;
-    }          
+  & img {
+    margin-left: auto;
+  }
+
+  @media screen and (max-width: 991px) {
+    width: 100%;
+  }
 `
 
 export const LightPart = styled.section`
-    width: 50%;
-    padding: 3rem 0;
-    background-color: #f8f8f8;
-    color: #1D1D1D;
+  width: 50%;
+  padding: 3rem 0;
+  background-color: #f8f8f8;
+  color: #1D1D1D;
 
-     
-    & div {
-        display: flex;
-        justify-content: center;
-        margin: 3rem;
-    }
 
-    @media screen and (max-width: 991px) {
-        width: 100%;
-    }
+  & div {
+    display: flex;
+    justify-content: center;
+    margin: 3rem;
+  }
+
+  @media screen and (max-width: 991px) {
+    width: 100%;
+  }
 `
